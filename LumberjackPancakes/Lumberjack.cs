1de57953af5c93adfe869dfd59c776675c95f2a0
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-08 09:45:36 +0200

    Added 'Lumberjack' class

---
+++ LumberjackPancakes/Lumberjack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LumberjackPancakes
+{
+    internal class Lumberjack
+    {
+        public string Name { get; private set; }
+        private Stack<Flapjack> flapjackStack = new Stack<Flapjack> ();
+
+        public Lumberjack(string name, Stack<Flapjack> flapjackStack)
+        {
+            Name = name;
+            this.flapjackStack = flapjackStack;
+        }
+
+        private void TakeFlapjack(Flapjack flapjack)
+        {
+            flapjackStack.Push (flapjack);
+        }
+
+        private void EatFlapjacks()
+        {
+            Console.WriteLine($"{this.Name} je naleśniki.");
+            flapjackStack.Pop ();
+            
+        }
+    }
+}
